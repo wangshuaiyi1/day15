26b2f6c7eaed8763db7837282938eb38346dad30
wangasdfghjk <13834834+wangasdfghjk@user.noreply.gitee.com>
2024-08-02 08:49:34 +0800

    wsy

---
+++ grpc/grpc.go
@@ -0,0 +1,27 @@
+package grpc
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+func RegisterGrpc(port int, Server func(server *grpc.Server)) error {
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	Server(s)
+	reflection.Register(s)
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	return nil
+}
